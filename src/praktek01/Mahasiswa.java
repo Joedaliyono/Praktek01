aae0868a2daaaa1a564d2d6f10616c96cc42649f
Joedaliyono <119489915+Joedaliyono@users.noreply.github.com>
2022-12-16 23:09:21 +0800

    Membuat Attribut Behavior

--- src/praktek01/Mahasiswa.java
+++ src/praktek01/Mahasiswa.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author User
  */
 public class Mahasiswa {
+    string nama;
+    string NPM;
+    double IPK;
+    
+    
+    void cetakInfo(){
+        System.out.println("Nama :"+nama);
+         System.out.println("NPM :"+NPM);
+          System.out.println("IPK :"+IPK);
+          
+        
+    }
     
 }
